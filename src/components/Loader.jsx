5c8c2ab9bc8d39783e207e726a88c7d59118e9f1
AmirAlipoor-ir <89448149+AmirAlipoor-ir@users.noreply.github.com>
2023-09-17 18:38:47 +0330

    add loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,10 @@
+import { LoaderIcon } from "react-hot-toast"
 
 function Loading() {
   return (
-    <div>
-    is loading
+    <div className="flex items-center gap-3">
+    <p>is loading</p>
+    <LoaderIcon className="w-5 h-5"/>
     </div>
   )
 }
